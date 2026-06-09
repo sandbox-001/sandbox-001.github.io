a66f2481352e5cd79a5a42cdbbe17bc9d6ba5772
djjeong127 <djjeong127@gmail.com>
2026-06-09 19:20:33 -0400

    feat: reorganize file structure

--- sandbox-001/src/app/app.routes.ts
+++ sandbox-001/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Homepage } from './features/homepage/homepage';
-import { Christmas } from './features/christmas/christmas/christmas';
-import { MoviesAndShows } from './features/movies-and-shows/movies-and-shows/movies-and-shows';
+import { Christmas } from './features/christmas/christmas';
+import { MoviesAndShows } from './features/movies-and-shows/movies-and-shows';
 import { NotFound } from './shared/components/not-found/not-found';
 
 export const routes: Routes = [
