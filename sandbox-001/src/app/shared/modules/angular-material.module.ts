0593f8b8826f110ceb1acab4b4440ebf2a70f998
djjeong127 <djjeong127@gmail.com>
2026-06-12 20:14:00 -0400

    feat: tidy up UI of recipes feature

--- sandbox-001/src/app/shared/modules/angular-material.module.ts
+++ sandbox-001/src/app/shared/modules/angular-material.module.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatRippleModule } from '@angular/material/core';
 
 export const ANGULAR_MATERIAL_MODULES = [
     MatToolbarModule,
@@ -10,4 +12,6 @@ export const ANGULAR_MATERIAL_MODULES = [
     MatIconModule,
     MatProgressSpinnerModule,
     MatCardModule,
+    MatTooltipModule,
+    MatRippleModule,
 ]
