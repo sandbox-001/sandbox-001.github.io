73cea9f8ca96243dea8cff0315ebb5b7d0f0fbba
djjeong127 <djjeong127@gmail.com>
2026-06-19 18:14:20 -0400

    feat: fixed some movie/tv filter header ui alignments and got a custom mode for the movies/tv feature

--- sandbox-001/src/app/app.ts
+++ sandbox-001/src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { Header } from "./shared/components/header/header";
+import { filter } from 'rxjs';
+import { Feature, ThemeService } from './shared/services/theme-service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,27 @@ import { Header } from "./shared/components/header/header";
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('sandbox-001');
+
+  router = inject(Router)
+  themeService = inject(ThemeService)
+
+  constructor() {
+    this.setupFeatureModeRouteListener()
+  }
+
+  // pays attention to the route url and change the Featuremode
+  private setupFeatureModeRouteListener() {
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd) => {
+
+      // switch feature mode
+      if (event.urlAfterRedirects.includes('/movies-and-shows')) {
+        this.themeService.switchFeature(Feature.MoviesAndShows)
+      }
+      else {
+        this.themeService.switchFeature(Feature.DefaultAll)
+      }
+
+    });
+  }
 }
--- sandbox-001/src/app/shared/services/theme-service.ts
+++ sandbox-001/src/app/shared/services/theme-service.ts
@@ -6,6 +6,17 @@ export enum Modes {
   Dark = 'dark'
 }
 
+// Closed set of features (for toggling light/dark mode)
+export enum Feature {
+    DefaultAll = 'default-for-all',
+    MoviesAndShows = 'movies-and-shows'
+}
+
+export interface FeatureMode {
+    feature: Feature;
+    mode: Modes;
+}
+
 // Closed set of specific user roles
 export enum Themes {
   Mountain = 'mountain',
@@ -13,23 +24,32 @@ export enum Themes {
   SeoulCherryBlossom = 'seoul-cherry-blossom'
 }
 
-//산
-//제주
-//벚꽃
 
 @Service()
 export class ThemeService {
     private document = inject(DOCUMENT);
-    
-    public mode = signal(Modes.Light);
+
+    private defaultAllFeatureMode: FeatureMode = {
+        feature: Feature.DefaultAll,
+        mode: Modes.Light
+    }
+    private defaultMovieFeatureMode: FeatureMode = {
+        feature: Feature.MoviesAndShows,
+        mode: Modes.Dark
+    }
+    private listOfDefaultFeatureModes: FeatureMode[] = [this.defaultAllFeatureMode, this.defaultMovieFeatureMode]
+
+
+    public currentFeatureMode = signal<FeatureMode>(this.defaultAllFeatureMode)
     public theme = signal(Themes.Mountain);
 
-    private setMode(mode: Modes) {
+
+    private setFeatureMode(featureMode: FeatureMode) {
         const root = this.document.documentElement;
 
-        this.mode.update(() => mode)
-        root.style.setProperty('--color-scheme', this.mode())
-        localStorage.setItem('mode', this.mode());
+        this.currentFeatureMode.set(featureMode)
+        root.style.setProperty('--color-scheme', this.currentFeatureMode().mode)
+        localStorage.setItem(`${this.currentFeatureMode().feature}-mode`, JSON.stringify(this.currentFeatureMode()));
     }
 
     private setTheme(theme: Themes) {
@@ -45,15 +65,23 @@ export class ThemeService {
         root.classList.add(this.theme())
         localStorage.setItem('theme', this.theme());
     }
-    
+
+    public switchFeature(feature: Feature) {
+        const storedFeaureMode: FeatureMode = JSON.parse(localStorage.getItem(`${feature}-mode`)!)
+
+        this.setFeatureMode(storedFeaureMode)
+    }
+
     public toggleMode() {
         const root = this.document.documentElement;
 
-        if (this.mode() === Modes.Light) {
-            this.setMode(Modes.Dark)
+        if (this.currentFeatureMode().mode === Modes.Light) {
+            this.currentFeatureMode.update((featureMode) => ({...featureMode, mode: Modes.Dark}))
+            this.setFeatureMode(this.currentFeatureMode())
         }
         else {
-            this.setMode(Modes.Light)
+            this.currentFeatureMode.update((featureMode) => ({...featureMode, mode: Modes.Light}))
+            this.setFeatureMode(this.currentFeatureMode())
         }
     }
 
@@ -72,19 +100,34 @@ export class ThemeService {
     }
 
     constructor() {
-        if (localStorage.getItem('mode')) {
-            this.setMode(localStorage.getItem('mode')! as Modes)
-        }
-        else {
-            this.setMode(this.mode())
-        }
+        const features: Feature[] = Object.values(Feature)
 
+        // initialize FeatureModes in localstorage
+        features.forEach((feature) => {
+            if (localStorage.getItem(`${feature}-mode`)) {
+                console.log('exists')
+                const storedFeatureMode = JSON.parse(localStorage.getItem(`${feature}-mode`)!)
+                this.setFeatureMode(storedFeatureMode)
+            }
+            else {
+                console.log('does not exist')
+                this.setFeatureMode(this.listOfDefaultFeatureModes.find((featureMode) => featureMode.feature === feature)!)
+            }
+        })
+
+        // after initializing, remember to switch the feature to the default
+        this.switchFeature(Feature.DefaultAll)
+
+        // and then we can also switch the feature by tracking the current route in the root app-root component
+        // REMEMBER TO ALSO EDIT THE ROUTE CHECKS IN APP-ROOT COMPONENT
+
+        // initialize themes in localstorage
         if (localStorage.getItem('theme')) {
             this.setTheme(localStorage.getItem('theme')! as Themes)
         }
         else {
             this.setTheme(this.theme())
         }
-        
+
     }
 }
