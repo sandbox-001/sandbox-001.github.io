c58f9f7d79e7e28e40e6408df349fa16177653e5
djjeong127 <djjeong127@gmail.com>
2026-07-21 17:28:03 -0400

    feat: added service for getting payroll taxes

--- sandbox-001/src/app/features/finance-calculators/models/calculator.model.ts
+++ sandbox-001/src/app/features/finance-calculators/models/calculator.model.ts
@@ -57,3 +57,97 @@ export interface MortgageCalculationStats {
     principal: number;
     remainingBalance: number;
 }
+
+export interface PayrollTaxApiRequest {
+    workState: State,
+    payDate: Date,
+    residenceState: State,
+    grossWages: number,
+    payPeriod: PayPeriod,
+    filingStatus: FilingStatus,
+    allowances: number
+}
+
+export interface PayrollTaxApiResponse {
+    taxes: Tax[]
+}
+
+export interface Tax {
+    category: string
+    effective_date: string,
+    jurisdiction: string,
+    name: string,
+    rate: number
+    rate_structure: string,
+    supplemental_rate: number
+    tax_type_code: string
+    taxpayer_side: string
+    wage_base: number
+}
+
+export enum FilingStatus {
+    Single = 'single',
+    Married = 'married',
+    Head_of_household = 'head_of_household'
+}
+
+export enum PayPeriod {
+    Weekly = 'weekly',
+    Biweekly = 'biweekly',
+    Semimonthly = 'semimonthly',
+    Monthly = 'monthly',
+    Annual = 'annual'
+}
+
+export enum State {
+    Alabama = 'AL',
+    Alaska = 'AK',
+    Arizona = 'AZ',
+    Arkansas = 'AR',
+    California = 'CA',
+    Colorado = 'CO',
+    Connecticut = 'CT',
+    Delaware = 'DE',
+    Florida = 'FL',
+    Georgia = 'GA',
+    Hawaii = 'HI',
+    Idaho = 'ID',
+    Illinois = 'IL',
+    Indiana = 'IN',
+    Iowa = 'IA',
+    Kansas = 'KS',
+    Kentucky = 'KY',
+    Louisiana = 'LA',
+    Maine = 'ME',
+    Maryland = 'MD',
+    Massachusetts = 'MA',
+    Michigan = 'MI',
+    Minnesota = 'MN',
+    Mississippi = 'MS',
+    Missouri = 'MO',
+    Montana = 'MT',
+    Nebraska = 'NE',
+    Nevada = 'NV',
+    New_Hampshire = 'NH',
+    New_Jersey = 'NJ',
+    New_Mexico = 'NM',
+    New_York = 'NY',
+    North_Carolina = 'NC',
+    North_Dakota = 'ND',
+    Ohio = 'OH',
+    Oklahoma = 'OK',
+    Oregon = 'OR',
+    Pennsylvania = 'PA',
+    Rhode_Island = 'RI',
+    South_Carolina = 'SC',
+    South_Dakota = 'SD',
+    Tennessee = 'TN',
+    Texas = 'TX',
+    Utah = 'UT',
+    Vermont = 'VT',
+    Virginia = 'VA',
+    Washington = 'WA',
+    West_Virginia = 'WV',
+    Wisconsin = 'WI',
+    Wyoming = 'WY'
+}
